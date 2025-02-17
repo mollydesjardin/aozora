7909ab9d055be13d6c577c77f5ea7799b061fc19
Molly Des Jardin <2146722+mollydesjardin@users.noreply.github.com>
2025-02-17 11:52:57 -0500

    Updated code style to conform to PEP8 and make variable naming consistent

--- aozora.py
+++ aozora.py
@@ -3,12 +3,12 @@
 
 """
 Aozora Corpus Builder (aozora.py)
-This script converts Aozora Bunko HTML files to UTF-8 plain-text, word-tokenized versions
-using MeCab and Beautiful Soup.
+This script converts Aozora Bunko HTML files to UTF-8 plain-text,
+word-tokenized versions using MeCab and Beautiful Soup.
 Author: Molly Des Jardin
 License: MIT
 """
- 
+
 from datetime import datetime, timezone
 from bs4 import BeautifulSoup as bs
 from pathlib import Path
@@ -17,11 +17,11 @@ import csv
 import re
 import MeCab
 
-rubytags = ['rt', 'rp']
-localpath = 'aozorabunko_html/cards/'
-sourceurl = 'https://www.aozora.gr.jp'
-outpath = Path.cwd().joinpath('tokenized')
-sourcecsv = 'list_person_all_extended_utf8.csv'
+ruby_tags = ['rt', 'rp']
+local_path = 'aozorabunko_html/cards/'
+source_url = 'https://www.aozora.gr.jp'
+out_path = Path.cwd().joinpath('tokenized')
+source_csv = 'list_person_all_extended_utf8.csv'
 
 # Create the tagger to reuse for all texts
 # (explicitly ignore config with -r /dev/null, specify dictionary with -d)
@@ -33,6 +33,7 @@ tagger = MeCab.Tagger('-r ' + os.devnull + ' -d 60a_kindai-bungo -Owakati')
 result_metadata = {}
 files = []
 
+
 def init_metadata():
     """
     Initializes result_metadata{} with header from Aozora CSV.
@@ -41,25 +42,26 @@ def init_metadata():
     result_metadata (using local path as key). Filenames are stored in files[].
     """
 
-    with open (sourcecsv, newline='') as csvin:
-        csvreader = csv.reader(csvin)
+    with open(source_csv, newline='') as csvin:
+        csv_reader = csv.reader(csvin)
 
-        result_metadata['header'] = next(csvreader)
+        result_metadata['header'] = next(csv_reader)
         result_metadata['header'].append('Tokenized Filename')
         result_metadata['header'].append('Time Processed (UTC)')
 
-        for row in csvreader:
-            if sourceurl in row[50]:
+        for row in csv_reader:
             # Only store data for files hosted at Aozora URL
-                filepath = '-'.join(row[50].split('/')[4:])
-                if filepath not in files:
-                    files.append(filepath)
-                    result_metadata[filepath] = row
-                    
+            if source_url in row[50]:
+                file_path = '-'.join(row[50].split('/')[4:])
+                if file_path not in files:
+                    files.append(file_path)
+                    result_metadata[file_path] = row
+
 
 def ruby_replace(matchobj):
     """
-    Find ruby annotation pattern for non-standard files, using matchobj regular expression.
+    Find ruby annotation pattern for non-standard files, using matchobj
+    regular expression.
     Return the first (0th) string extracted from the ruby pattern, which
     is the inline text (not gloss or punctuation).
     """
@@ -67,7 +69,7 @@ def ruby_replace(matchobj):
     return matchobj.string[4:].split('（')[0]
 
 
-def plaintext(f):
+def to_plain_text(f):
     """
     Removes ruby (annotation and gloss) and HTML markup tags.
     If successful, returns plain text string of work content.
@@ -75,17 +77,17 @@ def plaintext(f):
     """
 
     with open(f, mode='r', encoding='Shift-JIS', errors='ignore') as fin:
-        filetext = fin.read()
-        
+        file_text = fin.read()
+
     # Delete excess <br /> present in older files that don't have <p> tags,
     # to prevent output from having excessive line-break whitespace.
-    filetext = filetext.replace("<br />", "")
+    file_text = file_text.replace("<br />", "")
 
-    soup = bs(filetext, "html5lib").select(".main_text")
+    soup = bs(file_text, "html5lib").select(".main_text")
 
     # Default case, use Beautiful Soup parser to remove ruby, return text
     if len(soup) == 1:
-        for tag in soup[0].find_all(rubytags):
+        for tag in soup[0].find_all(ruby_tags):
             tag.extract()
         return soup[0].text
 
@@ -93,8 +95,8 @@ def plaintext(f):
     #   - Use regex match to retain glossed word without ruby or punctuation
     #   - Use Beautiful Soup parser to return text within <body> tag
     elif len(soup) == 0:
-        nonruby = re.sub(r"<!R>.*?（.*?）", ruby_replace, filetext)
-        soup = bs(nonruby, "html5lib").find("body")
+        non_ruby = re.sub(r"<!R>.*?（.*?）", ruby_replace, file_text)
+        soup = bs(non_ruby, "html5lib").find("body")
         return soup.text
 
     # Skip processing for other unexpected cases
@@ -104,36 +106,40 @@ def plaintext(f):
 
 def main():
 
-    if (not(outpath.exists())):
-        outpath.mkdir()    
+    if not (out_path.exists()):
+        out_path.mkdir()
     init_metadata()
 
     for filename in files:
-        inpath = Path.cwd().joinpath(localpath + filename.replace('-', '/'))
+        in_path = Path.cwd().joinpath(local_path + filename.replace('-', '/'))
 
         # 1. Remove ruby
         # 2. Get only "main" work text (no HTML tags or metadata)
-        if inpath.is_file():
-            text = plaintext(inpath)
+        if in_path.is_file():
+            text = to_plain_text(in_path)
 
         # 3. Tokenize using MeCab & save output txt file
             if text:
-                textlines = text.split('\n')
-                parsedlines = [tagger.parse(line).strip() for line in textlines]
-                parsed = '\n'.join(parsedlines).strip()
-                outfilename = 't-' + filename[:-5] + '.txt'
-                with open(outpath.joinpath(outfilename), mode='w', encoding='utf-8') as fout:
-                    fout.write(parsed)
-                result_metadata[filename].append(outfilename)
-                result_metadata[filename].append(str(datetime.now(timezone.utc)))
+                text_lines = text.split('\n')
+                parsed_lines = [tagger.parse(line).strip() for line in
+                                text_lines]
+                parsed_full = '\n'.join(parsed_lines).strip()
+                out_filename = 't-' + filename[:-5] + '.txt'
+                with open(out_path.joinpath(out_filename), mode='w',
+                          encoding='utf-8') as fout:
+                    fout.write(parsed_full)
+                result_metadata[filename].append(out_filename)
+                result_metadata[filename].append(str(datetime.now(
+                    timezone.utc)))
 
     # Save CSV with all original Aozora metadata per each file (row), plus
     # output filename and processing timestamp as extra columns
-    outcsv = Path.cwd().joinpath('t-list_person_all_extended_utf8.csv')
-    with open(outcsv, mode='w', encoding='utf-8') as fout:
+    out_csv = Path.cwd().joinpath('t-list_person_all_extended_utf8.csv')
+    with open(out_csv, mode='w', encoding='utf-8') as fout:
         w = csv.writer(fout)
         w.writerow(result_metadata['header'])
         for filename in files:
             w.writerow(result_metadata[filename])
+
 
 main()
