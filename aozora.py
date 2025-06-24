10b07ad0598ffaaa702fe5e1cca4ad5dc57e336d
Molly Des Jardin <2146722+mollydesjardin@users.noreply.github.com>
2025-06-24 15:23:49 -0400

    Double-quoted strings made consistent throughout

--- aozora.py
+++ aozora.py
@@ -20,14 +20,14 @@ import MeCab
 from bs4 import BeautifulSoup as bs
 from bs4 import XMLParsedAsHTMLWarning
 
-ruby_pattern = re.compile('<ruby><rb>.*?</rb><rp>.*?</ruby>')
-ruby_pattern_old = re.compile('<!R>.*?（.*?）')
-ruby = {'start': '<ruby><rb>', 'end': '</rb>'}
-ruby_old = {'start': '<!R>', 'end': '（'}
-local_path = 'aozorabunko_html/cards/'
-dict_path = '60a_kindai-bungo'
-out_csv = 't-list_person_all_extended_utf8.csv'
-out_path = Path.cwd().joinpath('tokenized')
+ruby_pattern = re.compile("<ruby><rb>.*?</rb><rp>.*?</ruby>")
+ruby_pattern_old = re.compile("<!R>.*?（.*?）")
+ruby = {"start": "<ruby><rb>", "end": "</rb>"}
+ruby_old = {"start": "<!R>", "end": "（"}
+local_path = "aozorabunko_html/cards/"
+dict_path = "60a_kindai-bungo"
+out_csv = "t-list_person_all_extended_utf8.csv"
+out_path = Path.cwd().joinpath("tokenized")
 
 def init_metadata(source_url:str="https://www.aozora.gr.jp/cards",
                   source_csv:str="list_person_all_extended_utf8.csv",
@@ -52,13 +52,13 @@ def init_metadata(source_url:str="https://www.aozora.gr.jp/cards",
 
     metadata = {}
 
-    with (open(source_csv, newline='') as csvin):
+    with (open(source_csv, newline="") as csvin):
         csv_reader = csv.reader(csvin)
 
         header_row = next(csv_reader)
         html_column = header_row.index(source_path)
-        header_row.append('Tokenized Filename')
-        metadata['header'] = header_row
+        header_row.append("Tokenized Filename")
+        metadata["header"] = header_row
 
         for row in csv_reader:
             # Only store data for files hosted at Aozora URL
@@ -85,10 +85,10 @@ def strip_ruby(text: str, ) -> str:
 
     """
 
-    if ruby['start'] in text:
+    if ruby["start"] in text:
         return re.sub(ruby_pattern, ruby_replace, text)
 
-    elif ruby_old['start'] in text:
+    elif ruby_old["start"] in text:
         return re.sub(ruby_pattern_old, ruby_replace_old, text)
 
     else:
@@ -111,7 +111,7 @@ def ruby_replace(matchobj: Match) -> str:
 
     """
 
-    return matchobj.group(0).lstrip(ruby['start']).split(ruby['end'])[0]
+    return matchobj.group(0).lstrip(ruby["start"]).split(ruby["end"])[0]
 
 
 def ruby_replace_old(matchobj: Match) -> str:
@@ -130,8 +130,8 @@ def ruby_replace_old(matchobj: Match) -> str:
 
     """
 
-    return matchobj.group(0).lstrip(ruby_old['start']).split(ruby_old[
-                                                                 'end'])[0]
+    return matchobj.group(0).lstrip(ruby_old["start"]).split(ruby_old[
+                                                                 "end"])[0]
 
 
 def to_plain_text(html_text: str) -> str:
@@ -149,22 +149,22 @@ def to_plain_text(html_text: str) -> str:
     """
 
     # Clean up <br /> to avoid excessive line breaks in output
-    html_text = html_text.replace('<br />', '')
+    html_text = html_text.replace("<br />", "")
 
     # Aozora standard HTML contains exactly ONE div with "main_text" class
     # (used like an ID), so test for 1 result and return markup-stripped text
-    soup = bs(html_text, 'html5lib').select('.main_text')
+    soup = bs(html_text, "html5lib").select(".main_text")
     if len(soup) == 1:
         return soup[0].text
 
     # For older files, return markup-stripped text from <body>
     elif len(soup) == 0:
-        soup = bs(html_text, 'html5lib').find('body')
+        soup = bs(html_text, "html5lib").find("body")
         if soup:
             return soup.text
 
     # Skip processing for files with unexpected structure
-    return ''
+    return ""
 
 
 def main():
@@ -173,9 +173,9 @@ def main():
     metadata: Dict[str, List[str]] = init_metadata()
 
     # Create MeCab tagger to reuse for all texts
-    tagger = MeCab.Tagger(f'-r {os.devnull!s} -d {dict_path!s} -Owakati')
+    tagger = MeCab.Tagger(f"-r {os.devnull!s} -d {dict_path!s} -Owakati")
 
-    # Suppress Beautiful Soup warnings that don't apply to these files
+    # Suppress Beautiful Soup warnings that don"t apply to these files
     warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
 
     for filename in metadata:
@@ -183,7 +183,7 @@ def main():
         f = Path.cwd().joinpath(local_path, filename)
 
         if f.is_file():
-            with (open(f, mode='r', encoding='Shift-JIS', errors='ignore') as
+            with (open(f, mode="r", encoding="Shift-JIS", errors="ignore") as
                   fin):
                 text = fin.read()
 
@@ -193,26 +193,26 @@ def main():
 
             if text:
                 # Tokenize using MeCab parser and rejoin text into one string
-                text_lines = text.split('\n')
-                parsed_text = '\n'.join([tagger.parse(line).strip() for line
+                text_lines = text.split("\n")
+                parsed_text = "\n".join([tagger.parse(line).strip() for line
                                          in text_lines]).strip()
 
                 # Write results out as .txt file
-                out_filename = filename.replace('html', 'txt')
-                out_filename = out_filename.replace('/', '-')
-                out_filename = f't-{out_filename!s}'
+                out_filename = filename.replace("html", "txt")
+                out_filename = out_filename.replace("/", "-")
+                out_filename = f"t-{out_filename!s}"
                 metadata[filename].append(out_filename)
-                with open(out_path.joinpath(out_filename), mode='w',
-                          encoding='utf-8') as fout:
+                with open(out_path.joinpath(out_filename), mode="w",
+                          encoding="utf-8") as fout:
                     fout.write(parsed_text)
 
     # Write out new, work-oriented CSV with added column for tokenized filename
-    with open(out_csv, mode='w', encoding='utf-8') as fout:
+    with open(out_csv, mode="w", encoding="utf-8") as fout:
         w = csv.writer(fout)
-        w.writerow(metadata.pop('header'))
+        w.writerow(metadata.pop("header"))
         for f in metadata:
             w.writerow(metadata[f])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
